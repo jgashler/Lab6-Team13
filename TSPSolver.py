71d8642a962bed53cfc2095be07c3631e3d8170f
Jack Gashler <jgashler44@gmail.com>
2022-11-30 12:33:13 -0700

    started playing with 3-swap. currently it is slower and less correct than 2-swap

--- TSPSolver.py
+++ TSPSolver.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 from which_pyqt import PYQT_VER
-if PYQT_VER == 'PYQT5':
-	from PyQt5.QtCore import QLineF, QPointF
-elif PYQT_VER == 'PYQT4':
-	from PyQt4.QtCore import QLineF, QPointF
-else:
-	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
+if PYQT_VER == 'PYQT5':
+    from PyQt5.QtCore import QLineF, QPointF
+elif PYQT_VER == 'PYQT4':
+    from PyQt4.QtCore import QLineF, QPointF
+else:
+    raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
 import time
 import numpy as np
@@ -17,122 +17,117 @@ import itertools
 import random
 
 
+def swap_elements(el1, el2):
+    return el2, el1
+
+
 class TSPSolver:
-	def __init__( self, gui_view ):
-		self._scenario = None
+    def __init__(self, gui_view):
+        self._scenario = None
 
-	def setupWithScenario( self, scenario ):
-		self._scenario = scenario
+    def setupWithScenario(self, scenario):
+        self._scenario = scenario
 
-	''' <summary>
-		This is the entry point for the default solver
-		which just finds a valid random tour.  Note this could be used to find your
-		initial BSSF.
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of solution, 
-		time spent to find solution, number of permutations tried during search, the 
-		solution found, and three null values for fields not used for this 
-		algorithm</returns> 
-	'''
+    def defaultRandomTour(self, time_allowance=60.0):
+        results = {}
+        cities = self._scenario.getCities()
+        ncities = len(cities)
+        foundTour = False
+        count = 0
+        bssf = None
+        start_time = time.time()
+        while not foundTour and time.time() - start_time < time_allowance:
+            # create a random permutation
+            perm = np.random.permutation(ncities)
+            route = []
+            # Now build the route using the random permutation
+            for i in range(ncities):
+                route.append(cities[perm[i]])
+            bssf = TSPSolution(route)
+            count += 1
+            if bssf.cost < np.inf:
+                # Found a valid route
+                foundTour = True
+        end_time = time.time()
+        results['cost'] = bssf.cost if foundTour else math.inf
+        results['time'] = end_time - start_time
+        results['count'] = count
+        results['soln'] = bssf
+        results['max'] = None
+        results['total'] = None
+        results['pruned'] = None
+        return results
 
-	def defaultRandomTour( self, time_allowance=60.0 ):
-		results = {}
-		cities = self._scenario.getCities()
-		ncities = len(cities)
-		foundTour = False
-		count = 0
-		bssf = None
-		start_time = time.time()
-		while not foundTour and time.time()-start_time < time_allowance:
-			# create a random permutation
-			perm = np.random.permutation( ncities )
-			route = []
-			# Now build the route using the random permutation
-			for i in range( ncities ):
-				route.append( cities[ perm[i] ] )
-			bssf = TSPSolution(route)
-			count += 1
-			if bssf.cost < np.inf:
-				# Found a valid route
-				foundTour = True
-		end_time = time.time()
-		results['cost'] = bssf.cost if foundTour else math.inf
-		results['time'] = end_time - start_time
-		results['count'] = count
-		results['soln'] = bssf
-		results['max'] = None
-		results['total'] = None
-		results['pruned'] = None
-		return results
+    def greedy(self, time_allowance=60.0):
+        pass
 
-	''' <summary>
-		This is the entry point for the greedy solver, which you must implement for 
-		the group project (but it is probably a good idea to just do it for the branch-and
-		bound project as a way to get your feet wet).  Note this could be used to find your
-		initial BSSF.
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
-		time spent to find best solution, total number of solutions found, the best
-		solution found, and three null values for fields not used for this 
-		algorithm</returns> 
-	'''
+    def branchAndBound(self, time_allowance=60.0):
+        pass
 
-	def greedy(self, time_allowance=60.0):
-		pass
+    def fancy(self, time_allowance=60.0):
+        cities = self._scenario.getCities()
+        ncities = len(cities)
+        count = 0
 
-	''' <summary>
-		This is the entry point for the branch-and-bound algorithm that you will implement
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
-		time spent to find best solution, total number solutions found during search (does
-		not include the initial BSSF), the best solution found, and three more ints: 
-		max queue size, total number of states created, and number of pruned states.</returns> 
-	'''
+        # TODO: actually implement greedy tour and use its solution here
+        soln = self.defaultRandomTour(time_allowance)['soln']
 
-	def branchAndBound(self, time_allowance=60.0):
-		pass
+        start = time.time()
 
-	''' <summary>
-		This is the entry point for the algorithm you'll write for your group project.
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
-		time spent to find best solution, total number of solutions found during search, the 
-		best solution found.  You may use the other three field however you like.
-		algorithm</returns> 
-	'''
+        # TODO: for some reason, adding this iteration loop makes it so the final solution does not draw lines. works otherwise
+        for iteration in range(2000):
+            # TODO: improve randomization. this helps, but is not perfect
+            for city in range(random.randint(0, ncities), ncities):
+                for neighbor in range(ncities):
+                    route_to_check = soln.route
 
-	def fancy(self, time_allowance=60.0):
-		cities = self._scenario.getCities()
-		ncities = len(cities)
-		count = 0
+                    # swap two paths
+                    temp = route_to_check[city]
+                    route_to_check[city] = route_to_check[neighbor]
+                    route_to_check[neighbor] = temp
 
-		# TODO: actually implement greedy tour and use its solution here
-		soln = self.defaultRandomTour(time_allowance)['soln']
+                    route_to_check = TSPSolution(route_to_check)
 
-		start = time.time()
+                    # if better than current solution, replace
+                    if route_to_check.cost < soln.cost:
+                        soln = route_to_check
+                        count += 1
 
-		# TODO: for some reason, adding this iteration loop makes it so the final solution does not draw lines. works otherwise
-		for iteration in range(2000):
-			# TODO: improve randomization. this helps, but is not perfect
-			for city in range(random.randint(0, ncities), ncities):
-				for neighbor in range(ncities):
-					route_to_check = soln.route
+        finish = time.time()
 
-					# swap two paths
-					temp = route_to_check[city]
-					route_to_check[city] = route_to_check[neighbor]
-					route_to_check[neighbor] = temp
+        return {'cost': soln.cost, 'time': finish - start, 'count': count, 'soln': soln, 'max': None, 'total': None,
+                'pruned': None}
 
-					route_to_check = TSPSolution(route_to_check)
+    def fancy3(self, time_allowance=60):
+        cities = self._scenario.getCities()
+        ncities = len(cities)
+        count = 0
 
-					# if better than current solution, replace
-					if route_to_check.cost < soln.cost:
-						soln = route_to_check
-						count += 1
+        soln = self.defaultRandomTour(time_allowance)['soln']
 
-		finish = time.time()
+        start = time.time()
 
-		return {'cost': soln.cost, 'time': finish - start, 'count': count, 'soln': soln, 'max': None, 'total': None, 'pruned': None}
+        for c in range(100):
+            for city in range(ncities):
+                for neighbor1 in range(ncities):
+                    for neighbor2 in range(ncities):
+                        routes = []
 
+                        route1, route2, route3 = soln.route, soln.route, soln.route
+                        route1[city], route1[neighbor1] = swap_elements(route1[neighbor1], route1[city])  # 0 1
+                        route2[city], route2[neighbor2] = swap_elements(route2[neighbor2], route2[city])  # 0 2
+                        route3[neighbor1], route3[neighbor2] = swap_elements(route3[neighbor2], route3[neighbor1])  # 1 2
 
+                        routes.append(TSPSolution(route1))
+                        routes.append(TSPSolution(route2))
+                        routes.append(TSPSolution(route3))
 
+                        for route in routes:
+                            if route.cost < soln.cost:
+                                soln = route
+                                count += 1
+
+        finish = time.time()
+
+        return {'cost': soln.cost, 'time': finish - start, 'count': count, 'soln': soln, 'max': None, 'total': None,
+                'pruned': None}
